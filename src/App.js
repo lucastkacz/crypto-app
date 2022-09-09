ae04daf6c181d8e9947f36b782583449ef3b4913
lucas_wsl <lucastkacz@gmail.com>
2022-09-09 16:23:45 -0300

    charting

--- src/App.js
+++ src/App.js
@@ -2,27 +2,40 @@ import React from "react";
 import "./App.css";
 
 import { DataWebhookProvider } from "./context/DataWebhookContext";
+import { DataProvider } from "./context/DataContext";
 
 import BinanceStream from "./components/BinanceStream";
 import CurrentTime from "./components/CurrentTime";
 import FundingTime from "./components/FundingTime";
 import Chart from "./components/Chart";
-import Test from "./components/Test";
 
 function App() {
 	return (
 		<div>
-			{/* <CurrentTime></CurrentTime>
-			<DataWebhookProvider ticker="btcusdt">
-				<FundingTime></FundingTime>
-			</DataWebhookProvider>
+			<div className="top-bar">
+				<CurrentTime></CurrentTime>
 
-			<DataWebhookProvider ticker="btcusdt">
+				<DataWebhookProvider ticker="btcusdt">
+					<FundingTime></FundingTime>
+				</DataWebhookProvider>
+			</div>
+
+			<DataWebhookProvider ticker="1000shibusdt">
 				<BinanceStream></BinanceStream>
 			</DataWebhookProvider>
 
-			<Chart></Chart> */}
-			<Test></Test>
+			<DataProvider ticker="NEOUSDT">
+				<Chart></Chart>
+			</DataProvider>
+			<DataProvider ticker="LINKUSDT">
+				<Chart></Chart>
+			</DataProvider>
+			<DataProvider ticker="ADAUSDT">
+				<Chart></Chart>
+			</DataProvider>
+			<DataProvider ticker="XRPUSDT">
+				<Chart></Chart>
+			</DataProvider>
 		</div>
 	);
 }
--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,13 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 const DataContext = createContext({});
 
-const endpointUrl = "https://api.binance.com/api/v3/uiKlines?symbol=BTCUSDT&interval=1h&limit=1000";
-
 export const DataProvider = (props) => {
-	const fetchApi = async () => {
-		const response = await fetch(endpointUrl);
-		console.log(response.statusText);
-	};
+	const [klines, setKlines] = useState([]);
+	let ticker = props.ticker;
+
+	const endpointUrl = `https://api.binance.com/api/v3/uiKlines?symbol=${props.ticker}&interval=5m&limit=1000`;
+
+	useEffect(() => {
+		const fetchApi = async () => {
+			const response = await fetch(endpointUrl);
+			const responseData = await response.json();
+
+			if (responseData) {
+				for (let i = 0; i < responseData.length; i++) {
+					let kline = {
+						time: responseData[i][0] / 1000,
+						open: responseData[i][1],
+						high: responseData[i][2],
+						low: responseData[i][3],
+						close: responseData[i][4],
+					};
+					setKlines((prev) => [...prev, kline]);
+				}
+			}
+		};
+		fetchApi().catch(console.error);
+	}, [endpointUrl]);
+
+	return <DataContext.Provider value={{ klines, ticker }}>{props.children}</DataContext.Provider>;
 };
 
-// https://api.binance.com/api/v3/uiKlines?symbol=BTCUSDT&interval=1h&limit=1000
+export default DataContext;
